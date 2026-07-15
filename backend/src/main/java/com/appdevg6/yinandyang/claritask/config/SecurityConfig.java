02b4c8845dd8a2d794042664148ed6e97a29084a
Lenon Lee Natividad <lenonleenatividad4@gmail.com>
2026-07-16 02:45:37 +0800

    fix: allow all origin patterns dynamically to resolve CORS preflight errors

--- backend/src/main/java/com/appdevg6/yinandyang/claritask/config/SecurityConfig.java
+++ backend/src/main/java/com/appdevg6/yinandyang/claritask/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://eztasks-frontend.onrender.com"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
